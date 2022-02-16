8ba25ad96c82f72d82fda2d3a5923f38469c46f3
deven7818 <deven7818@gmail.com>
2022-02-16 11:23:16 +0530

    Emp wage daily wage Uc2

--- EmpWage/src/main/java/com/empwage/EmpWage.java
+++ EmpWage/src/main/java/com/empwage/EmpWage.java
@@ -7,15 +7,25 @@ public class EmpWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
 		
-		int isPresent = 1;
+		int isFullTime = 1;
+		int dailyWage;
+		int wagePerHr = 20;
+		int workingHr;
+		
+		
 		int empCheck= (int)(Math.floor(Math.random() *10)) % 2;
 		
-		if(empCheck == isPresent) {
+		if(empCheck == isFullTime) {
 			System.out.println("Employee is Present");
+			workingHr = 8;
 		}
 		else {
 			System.out.println("Employee is Absent");
+			workingHr = 0;
 		}
+		
+		dailyWage = workingHr * wagePerHr;
+		System.out.println("Empolyee daily wage is " + dailyWage);
 	}
 
 }
