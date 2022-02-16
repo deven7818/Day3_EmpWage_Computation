463b3e1434dd6cd3e07d94fd6a88890abfebbd76
deven7818 <deven7818@gmail.com>
2022-02-16 11:30:43 +0530

    Emp daily wage parttime,fulltime absent Uc3

--- EmpWage/src/main/java/com/empwage/EmpWage.java
+++ EmpWage/src/main/java/com/empwage/EmpWage.java
@@ -8,16 +8,21 @@ public class EmpWage {
 		System.out.println("Welcome to Employee Wage Computation");
 		
 		int isFullTime = 1;
+		int isPartTime = 2;
 		int dailyWage;
 		int wagePerHr = 20;
 		int workingHr;
 		
 		
-		int empCheck= (int)(Math.floor(Math.random() *10)) % 2;
+		int empCheck= (int)(Math.floor(Math.random() *10)) % 3;
 		
 		if(empCheck == isFullTime) {
-			System.out.println("Employee is Present");
+			System.out.println("Employee is full Present");
 			workingHr = 8;
+		}
+		else if(empCheck == isPartTime){
+			System.out.println("Employee is part Time present");
+			workingHr = 4;
 		}
 		else {
 			System.out.println("Employee is Absent");
